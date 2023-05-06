be77f38a4ba98e2636268a1662f7073995e503c1
mominur-emon <mominur.emon19@gmail.com>
2023-05-06 20:40:16 +0600

    Local Module - exports, require

---
+++ Node js/p1/student.js
@@ -0,0 +1,14 @@
+const getName = () => {
+  return "emon";
+};
+
+const getAge = () => {
+  return "25";
+};
+// exports.getName =getName;
+// exports.getAge=getAge;
+
+module.exports = {
+  getAge,
+  getName,
+};

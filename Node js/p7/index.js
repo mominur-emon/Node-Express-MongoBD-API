886b8b08ab104dd859911072bf9355ff9eecda24
Mominur Rahman Emon <109852788+mominur-emon@users.noreply.github.com>
2023-05-09 19:36:58 +0600

    http routing and nodemon package

---
+++ Node js/p7/index.js
@@ -0,0 +1,30 @@
+//npm install nodemon
+
+const http = require("http");
+const fs = require("fs");
+const PORT = 3000;
+const hostName = "127.0.0.1";
+
+const server = http.createServer((req, res) => {
+  const handleReadFile = (statusCode, fileLocation) => {
+    fs.readFile(fileLocation, (err, data) => {
+      res.writeHead(statusCode, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  };
+
+  if (req.url === "/") {
+    handleReadFile(200, "index.html");
+  } else if (req.url === "/about") {
+    handleReadFile(200, "about.html");
+  } else if (req.url === "/contact") {
+    handleReadFile(200, "contact.html");
+  } else {
+    handleReadFile(404, "error.html");
+  }
+});
+
+server.listen(PORT, hostName, () => {
+  console.log(`Server is running at http://${hostName}:${PORT}`);
+});

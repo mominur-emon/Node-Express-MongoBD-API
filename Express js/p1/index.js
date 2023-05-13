dfb1740515629e7c4a3133685a6fe8b125920582
Mominur Rahman Emon <109852788+mominur-emon@users.noreply.github.com>
2023-05-13 16:10:22 +0600

    create express server

---
+++ Express js/p1/index.js
@@ -0,0 +1,11 @@
+const app = require("./app");
+const PORT = "3000";
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+/* npm init -y
+npm install express nodemon
+npm start
+*/

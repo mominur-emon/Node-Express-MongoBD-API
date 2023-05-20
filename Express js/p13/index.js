e70ef1d0669db8ae684970e3c14972088d2b8828
Mominur Rahman Emon <109852788+mominur-emon@users.noreply.github.com>
2023-05-20 10:14:26 +0600

    MVC architecture

---
+++ Express js/p13/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const userRouter = require("./routes/users.route");
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(userRouter);
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "resource not found",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
---
+++ Express js/p13/models/users.model.js
@@ -0,0 +1,12 @@
+const users = [
+    {
+      name: "Mominur Rahman",
+      age: 24,
+    },
+    {
+      name: "Rifat Ahmed",
+      age: 22,
+    },
+  ];
+
+  module.exports = users;
